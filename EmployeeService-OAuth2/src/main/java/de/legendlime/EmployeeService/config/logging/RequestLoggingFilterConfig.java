b235429ce23fd7102db1c4c1cb34d270b28e30b4
Thomas Kautenburger <tkautenburger@legendlime.de>
2020-01-16 21:16:27 +0100

    Added request logging

---
+++ EmployeeService-OAuth2/src/main/java/de/legendlime/EmployeeService/config/logging/RequestLoggingFilterConfig.java
@@ -0,0 +1,21 @@
+package de.legendlime.EmployeeService.config.logging;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+ 
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter
+          = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(2000);
+        filter.setIncludeHeaders(true);
+        filter.setAfterMessagePrefix("REQUEST DATA : ");
+        return filter;
+    }
+}
